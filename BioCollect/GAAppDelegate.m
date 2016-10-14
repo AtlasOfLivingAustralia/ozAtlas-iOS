3c1238f3c48d4afd346a41de795a09183bff381b
sat01a <Sathish@Sathishs-iMac.local>
2016-10-14 14:11:03 +1100

    Code cleanup

--- BioCollect/GAAppDelegate.m
+++ BioCollect/GAAppDelegate.m
@@ -162,6 +162,7 @@
     aboutNC.tabBarItem.image = [UIImage imageNamed:@"about_filled-25"];
     aboutNC.navigationBar.topItem.title = @"About";
     
+
     //Tab bars
     tabBarController = [[UITabBarController alloc] init];
     NSArray* controllers = [NSArray arrayWithObjects: homeNC, sightingsNC, aboutNC,contactNC, nil];
@@ -169,14 +170,7 @@
 
     [self.window setRootViewController:tabBarController];
     [self.window makeKeyAndVisible];
-    
-    /*
-    if([[GASettings getEULA] length] == 0 && ![[GASettings getEULA]isEqualToString:kEULAAgreed]){
-        [self.window.rootViewController presentViewController:eulaVC animated:NO completion:nil];
-    }
-    else
-    */
-    
+
     if([GASettings getAuthKey] == 0){
         [self displaySigninPage];
         
@@ -208,120 +202,10 @@
 }
 
 -(NSString *) uploadChangedActivities : (NSError **) e{
-    int success = 0;
-    int failure = 0;
-
-    // Make sure site details are up to date.
-    for(GAProject *pro in self.projects) {
-        for(GASite *site in pro.sites){
-            if([site.permSiteId length] == 0 || site.permSiteId == nil) {
-                NSString *newSiteId = [self.restCall uploadSite :site: &*e];
-                DebugLog(@"[INFO] GAAppDelegate:uploadChangedActivities Server returned site id %@",newSiteId);
-                if([newSiteId length] >0 && *e == nil){
-                    site.permSiteId = newSiteId;
-                    [self.sqlLite updateProjectSites:site];
-                    [self.sqlLite updateSite:site];
-                } else{
-                    DebugLog(@"[ERROR] GAAppDelegate:uploadChangedActivities Site creation failed.");
-                }
-                
-            }
-        }
-    }
-     
-    for(GAProject *pro in self.projects) {
-        for(GAActivity *act in pro.activities){
-            if(act.status == ACTIVITY_CHANGED) {
-                //permSiteId provided by server, make sure we update the activity with the server perm site id.
-                if(act.site != nil && act.site.siteId != nil && act.site.permSiteId != nil && ![act.site.permSiteId isEqualToString:act.site.siteId]){
-                    DebugLog(@"[INFO] GAAppDelegate:uploadChangedActivities - Replacing temp site id with server provided site id.");
-                    act.activityJSON = [act.activityJSON stringByReplacingOccurrencesOfString:act.site.siteId withString:act.site.permSiteId];
-                }
-                [self.restCall updateActivity:act :&*e];
-                if(*e == nil) {
-                    success++;
-                }else{
-                    failure++;
-                }
-            }
-        }
-    }
-    
-    NSString *message = nil;
-    if((success+failure) == 0){
-        message = @"Successfully synced activities and sites";
-        DebugLog(@"[INFO] GAAppDelegate:uploadChangedActivities No changes to upload");
-    }
-    
-    else if (success > 0){
-        message = [[NSString alloc] initWithFormat: @"Successfully synced activities and sites."];
-        DebugLog(@"[SUCCESS] GAAppDelegate:uploadChangedActivities Sync successfull");
-    }
-    
-    else if (failure > 0){
-        message = [[NSString alloc] initWithFormat: @"Failed to sync"];
-        DebugLog(@"[ERROR] GAAppDelegate:uploadChangedActivities Faile to sync");
-    }
-return message;
+    return @"";
 }
 
 -(void) uploadAndDownload : (BOOL) enablePop{
-    [MRProgressOverlayView showOverlayAddedTo:self.window title:@"Syncing.." mode:MRProgressOverlayViewModeIndeterminate animated:YES];
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        NSError *uploadError = nil;
-        NSError *downloadError = nil;
-        
-        // Projects to restore in case of failed uploa or download.
-        NSMutableArray *p = [self.sqlLite loadProjectsAndActivities];
-        [self updateTableModelsAndViews:p];
-        
-        NSString *uploadMessage = [self uploadChangedActivities : &uploadError];
-        if([[uploadError localizedDescription] length] > 0)
-            DebugLog(@"[ERROR] GAAppDelegate:uploadAndDownload Upload Error Message %@",[uploadError localizedDescription]);
-        
-        NSMutableArray *downloadPro = nil;
-        if(uploadError == nil) {
-            downloadPro = [self.restCall downloadProjects:&downloadError];
-            if(downloadError == nil){
-                [self.sqlLite storeProjects:downloadPro];
-                [GASettings setDataToSync:kDataToSyncFalse];
-            }
-        }
-        dispatch_async(dispatch_get_main_queue(), ^{
-            
-            [MRProgressOverlayView dismissOverlayForView:self.window animated:NO];
-
-            if(downloadError == nil && uploadError == nil) {
-                [self updateTableModelsAndViews:downloadPro];
-                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Sync successful"
-                                                                message:uploadMessage
-                                                               delegate:self
-                                                      cancelButtonTitle:@"Dismiss"
-                                                      otherButtonTitles:nil];
-                [alert show];
-            }
-            else if (downloadError != nil){
-                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Sync unsuccessful"
-                                                                message:[downloadError localizedDescription]
-                                                               delegate:self
-                                                      cancelButtonTitle:@"Try again later"
-                                                      otherButtonTitles:nil];
-                [alert show];
-            }
-            else if (uploadError != nil){
-                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Sync unsuccessful"
-                                                                message:[uploadError localizedDescription]
-                                                               delegate:self
-                                                      cancelButtonTitle:@"Try again later"
-                                                      otherButtonTitles:nil];
-                [alert show];
-            }
-            
-           if(enablePop)
-               [self goBackToDetailViewController];
-        });
-    });
-
 }
 
 -(void) goBackToDetailViewController{
